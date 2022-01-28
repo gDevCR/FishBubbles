2f276ce977e600b96d705db7bb2c216d3a4a1044
GerSC <gersc10@gmail.com>
2022-01-28 15:02:14 -0600

    Clean code
    Clean code and add some changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Canvas Setup
+// Canvas Setup ////////////////////////////
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 
@@ -7,11 +7,12 @@ canvas.height = 500;
 ctx.font = '50px Georgia';
 
 let score = 0;
+let globalLife = 100;
 let gameFrame = 0;
 let gameSpeed = 1;
 let gameOver = false;
 
-// Mouse interactivity
+// Mouse interactivity ////////////////////////////
 let canvasPosition = canvas.getBoundingClientRect(); // Get margins from browser border
 const mouse = {
   x: canvas.width / 2,
@@ -29,142 +30,19 @@ canvas.addEventListener('mouseup', function () {
   mouse.click = false;
 });
 
-// Player logic
+// Player logic ////////////////////////////
+const player = new Player();
+
 const fishLeft = new Image();
 fishLeft.src = 'sprites/fishLeft.png';
 
 const fishRight = new Image();
 fishRight.src = 'sprites/fishRight.png';
 
-class Player {
-  constructor() {
-    this.x = canvas.width;
-    this.y = canvas.height / 2;
-    this.radius = 50; // Player element is a circle
-    this.angle = 0;
-    this.frameX = 0;
-    this.frameY = 0;
-    this.frame = 0;
-    this.spriteWidth = 498; // Quadrant from width Sprite
-    this.spriteHeight = 327; // Quadrant from height Sprite
-  }
-
-  update() {
-    const dx = this.x - mouse.x; // Distance of X
-    const dy = this.y - mouse.y; // Distance of Y
-
-    let theta = Math.atan2(dy, dx);
-    this.angle = theta;
-
-    if (mouse.x !== this.x) {
-      this.x -= dx / 30; // Speed in 30
-    }
-    if (mouse.y !== this.y) {
-      this.y -= dy / 30; // Speed in 30
-    }
-
-    if (gameFrame % 5 === 0) {
-      this.frame++;
-      if (this.frame >= 12) this.frame = 0;
-      if (this.frame === 3 || this.frame === 7 || this.frame === 11) {
-        this.frameX = 0;
-      } else {
-        this.frameX++;
-      }
-      if (this.frame < 3) this.frameY = 0;
-      else if (this.frame < 7) this.frameY = 1;
-      else if (this.frame < 11) this.frameY = 2;
-      else this.frameY = 0;
-    }
-  }
-
-  draw() {
-    if (mouse.click) {
-      ctx.lineWidth = 0.2;
-      ctx.beginPath();
-      ctx.moveTo(this.x, this.y);
-      ctx.lineTo(mouse.x, mouse.y);
-      ctx.stroke();
-    }
-    // ctx.fillStyle = 'red';
-    // ctx.beginPath();
-    // ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    // ctx.fill();
-    // ctx.closePath();
-
-    ctx.save();
-    ctx.translate(this.x, this.y);
-    ctx.rotate(this.angle);
-
-    if (this.x >= mouse.x) {
-      ctx.drawImage(
-        fishLeft,
-        this.frameX * this.spriteWidth,
-        this.frameY * this.spriteHeight,
-        this.spriteWidth,
-        this.spriteHeight,
-        0 - 60,
-        0 - 45,
-        this.spriteWidth / 4,
-        this.spriteHeight / 4,
-      );
-    } else {
-      ctx.drawImage(
-        fishRight,
-        this.frameX * this.spriteWidth,
-        this.frameY * this.spriteHeight,
-        this.spriteWidth,
-        this.spriteHeight,
-        0 - 60,
-        0 - 45,
-        this.spriteWidth / 4,
-        this.spriteHeight / 4,
-      );
-    }
-    ctx.restore();
-  }
-}
-
-const player = new Player();
-
-// Bubbles logic
+// Bubbles logic ////////////////////////////
 const bubblesArray = [];
 const bubbleImage = new Image();
 bubbleImage.src = 'sprites/bubble01.png';
-class Bubble {
-  constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = canvas.height + 100;
-    this.radius = 50;
-    this.speed = Math.random() * 5 + 1;
-    this.distance;
-    this.collitioned = false;
-    this.sound = Math.random() <= 0.5 ? 'sound1' : 'sound2';
-  }
-  update() {
-    this.y -= this.speed;
-
-    //Calculate distance
-    const dx = this.x - player.x;
-    const dy = this.y - player.y;
-    this.distance = Math.sqrt(dx * dx + dy * dy);
-  }
-  draw() {
-    // ctx.fillStyle = 'blue';
-    // ctx.beginPath();
-    // ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    // ctx.fill();
-    // ctx.closePath();
-    // ctx.stroke();
-    ctx.drawImage(
-      bubbleImage,
-      this.x - 65,
-      this.y - 65,
-      this.radius * 2.6,
-      this.radius * 2.6,
-    );
-  }
-}
 
 const bubbleSound1 = document.createElement('audio');
 bubbleSound1.src = 'audio/sound1.ogg';
@@ -209,7 +87,7 @@ function handleBubbles() {
   for (let i = 0; i < bubblesArray.length; i++) {}
 }
 
-// Repeat brackground
+// Repeat brackground ////////////////////////////
 const background = new Image();
 background.src = 'sprites/background1.png';
 
@@ -231,82 +109,32 @@ function handleBackground() {
   ctx.drawImage(background, BG.x2, BG.y, BG.width, BG.height);
 }
 
-// Enemy
+// Enemy ////////////////////////////
+const enemy1 = new Enemy();
 const enemyImage = new Image();
 enemyImage.src = 'sprites/enemy01.png';
 
-class Enemy {
-  constructor() {
-    this.x = canvas.width - 200;
-    this.y = Math.random() * (canvas.height - 150) + 90;
-    this.radius = 60;
-    this.speed = Math.random() * 2 + 2;
-    this.frame = 0;
-    this.frameX = 0;
-    this.frameY = 0;
-    this.spriteWidth = 418;
-    this.spriteHeight = 397;
-  }
-  draw() {
-    // ctx.fillStyle = 'red';
-    // ctx.beginPath();
-    // ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    // ctx.fill();
-    ctx.drawImage(
-      enemyImage,
-      this.frameX * this.spriteWidth,
-      this.frameY * this.spriteHeight,
-      this.spriteWidth,
-      this.spriteHeight,
-      this.x - 60,
-      this.y - 70,
-      this.spriteWidth / 3,
-      this.spriteHeight / 3,
-    );
-  }
-  update() {
-    this.x -= this.speed;
-    if (this.x < 0 - this.radius * 2) {
-      this.x = canvas.width + 200;
-      this.y = Math.random() * (canvas.height - 150) + 90;
-      this.speed = Math.random() * 2 + 2;
-    }
-    if (gameFrame % 5 === 0) {
-      this.frame++;
-      if (this.frame >= 12) this.frame = 0;
-      if (this.frame === 3 || this.frame === 7 || this.frame === 11) {
-        this.frameX = 0;
-      } else {
-        this.frameX++;
-      }
-      if (this.frame < 3) this.frameY = 0;
-      else if (this.frame < 7) this.frameY = 1;
-      else if (this.frame < 11) this.frameY = 2;
-      else this.frameY = 0;
-    }
-    //Collition with player
-    const dx = this.x - player.x;
-    const dy = this.y - player.y;
-    const distance = Math.sqrt(dx * dx + dy * dy);
-    if (distance < this.radius + player.radius) {
-      handleGameOver();
-    }
-  }
-}
-
-const enemy1 = new Enemy();
 function handleEnemies() {
   enemy1.draw();
   enemy1.update();
 }
 
+// Set info of game ////////////////////////////
+function setCurrentInfo() {
+  var scoreData = document.querySelector('#scoreData');
+  scoreData.textContent = score + ' pts';
+
+  var healthData = document.querySelector('#healthData');
+  healthData.textContent = globalLife;
+}
+
 function handleGameOver() {
   ctx.fillStyle = 'white';
-  ctx.fillText('GAME OVER, you reached score ' + score, 130, 250);
+  ctx.fillText('GAME OVER', 250, 250);
   gameOver = true;
 }
 
-// Animation loop
+// Animation loop ////////////////////////////
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   handleBackground();
@@ -316,8 +144,9 @@ function animate() {
   handleEnemies();
 
   // Info
-  ctx.fillStyle = 'black';
-  ctx.fillText('Score: ' + score, 10, 50);
+  // ctx.fillStyle = 'black';
+  // ctx.fillText('Score: ' + textContent , 10, 50);
+  setCurrentInfo();
 
   //console.log(gameFrame);
   gameFrame++; // Counter for establish periodic events to our game
